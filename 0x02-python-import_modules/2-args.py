c355198a07ece00394dc81f10f10aa391d10e1d6
pichu185 <pichu_otegui@hotmail.com>
2021-04-29 18:40:04 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     argc = len(sys.argv) - 1
     if argc == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif argc == 1:
         print("1 argument:")
     else:
