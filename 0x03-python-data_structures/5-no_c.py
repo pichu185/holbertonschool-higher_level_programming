23ddaed1a2983a281a533180377d7ab72a3909a0
pichu185 <pichu_otegui@hotmail.com>
2021-05-04 18:52:28 +0000

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+
+    no_c = ""
+    for i in range(len(my_string)):
+        if my_string[i] != "c" and my_string[i] != "C":
+            no_c += my_string[i]
+    return no_c
