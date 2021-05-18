72ecbcfd3a1a5b611a24e056f7e4a4e601454c38
pichu185 <pichu_otegui@hotmail.com>
2021-05-18 20:30:50 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class called Square"""
+
+
+class Square:
+    """Square class with size attribute"""
+    def __init__(self, size=0):
+        """size initialitation"""
+        self.__size = size
