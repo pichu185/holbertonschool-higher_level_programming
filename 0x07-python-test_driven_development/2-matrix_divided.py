4af3c2f808e6342275cf0c0accf7b6c0581a6872
Pichu185 <pichu_otegui@hotmail.comm>
2021-05-21 22:25:48 -0300

    hola

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""Function to divide a matrix by a constant"""
 
 
 def matrix_divided(matrix, div):
+    """This function takes a matrix and divides every element by div"""
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if type(matrix) is not list:
-        raise TypeError(
-            ("matrix must be a matrix (list of lists) of integers/floats")
-   
+    if type(matrix) is not list or matrix is None:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
    
