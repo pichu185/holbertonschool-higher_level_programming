88f7d53abfc9a3da07ed701bdf0f1f052e3e7cfd
Pichu185 <pichu_otegui@hotmail.comm>
2021-06-01 22:38:38 -0300

    advanced

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """class MyInt"""
+    
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""function that adds a new attribute to an object if it’s possible"""
+
+
