d7ee180d0e28fb035ae642aab9db947c4c4bc225
pichu185 <pichu_otegui@hotmail.com>
2021-06-03 00:29:03 +0000

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,12 +5,10 @@ from a “JSON file”:
 
 
 import json
-from os import waitpid
 
 
 def load_from_json_file(filename):
     """creates object from JSON file"""
 
-    with open(filename, mode="r", encoding="utf-8") as file:
-    return json.load(file)
-    
+    with open(filename, encoding="utf-8") as file:
+        return (json.load(file))
