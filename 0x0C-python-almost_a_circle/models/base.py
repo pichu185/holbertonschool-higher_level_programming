5bce30195ad3a5b444fc579ae7547bc6e7ad1bc8
Pichu185 <pichu_otegui@hotmail.comm>
2021-06-07 18:08:00 -0300

    Readme

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write the first class Base"""
+
+
+class Base:
+    """class Base"""
+
+    def __init__(self, id=None):
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+"""Write the first class Base"""
