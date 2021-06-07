5a77ae71cbbd2cc93b899b64766d24f60bb2af89
Pichu185 <pichu_otegui@hotmail.comm>
2021-06-07 18:24:38 -0300

    1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,4 +5,11 @@
 class Base:
     """class Base"""
 
+    __nb_objects = 0
+
     def __init__(self, id=None):
+        if id is not None:
+            self.if = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
