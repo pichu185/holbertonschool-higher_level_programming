d6713c228631f9be713619070bf35d846eb1aa60
Pichu185 <pichu_otegui@hotmail.comm>
2021-06-08 22:11:36 -0300

    11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,44 +22,10 @@ class Square(Rectangle):
     @property
     def size(self):
         """size method"""
-        return self.size
-
-    @property
-    def x(self):
-        """x method"""
-        return self.__x
-
-    @property
-    def y(self):
-        """y method"""
-        return self.__y
+        return self.width
 
     @size.setter
     def size(self, value):
-        """size method"""
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value <= 0:
-            raise ValueError("size must be > 0")
-        else:
-            self.size = value
+        self.width = value
+        self.height = value
 
-    @x.setter
-    def x(self, value):
-        """x method"""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
-
-    @y.setter
-    def y(self, value):
-        """y method"""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
