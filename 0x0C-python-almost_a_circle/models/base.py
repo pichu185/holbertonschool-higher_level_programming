62b68d60e3e8955b8747c971ccb31ce38933c6e0
Pichu185 <pichu_otegui@hotmail.comm>
2021-06-08 22:47:43 -0300

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,7 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects
+            self.id = Base.__nb_objects
 
 
 @staticmethod
