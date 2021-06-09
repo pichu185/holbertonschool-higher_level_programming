59f4c36de7ed3d1fb02f2ac88b5d2f7c12befe49
Pichu185 <pichu_otegui@hotmail.comm>
2021-06-08 23:02:07 -0300

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,11 +19,19 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-"""static method"""
 
-@staticmethod
-def to_json_string(list_dictionaries):
-    """returns the JSON string representation of list_dictionaries"""
-    if list_dictionaries is None or len(list_dictionaries) == 0:
-        return "[]"
-    return json.dumps(list_dictionaries)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """class method that writes the JSON string representation
+        of list_objs to a file:
+        """
+        
+        with open(filename, mode="w", encoding="utf-8") as file:
+            
