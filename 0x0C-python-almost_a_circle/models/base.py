e4db58821e0ae7c8d377a6c399d60c5991c03762
Pichu185 <pichu_otegui@hotmail.comm>
2021-06-08 22:38:31 -0300

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Write the first class Base"""
 
 
+import json
+
+
 class Base:
     """class Base"""
 
@@ -14,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+@staticmethod
+def to_json_string(list_dictionaries):
+    """returns the JSON string representation of list_dictionaries"""
+    if list_dictionaries is None or len(list_dictionaries) == 0:
+        return "[]"
+    return json.dumps(list_dictionaries)
+
