39eef3490db227a10f6b4b13b0ee07b476ec516b
pichu185 <pichu_otegui@hotmail.com>
2021-06-09 01:02:50 +0000

    10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,13 +9,12 @@ class Square(Rectangle):
     """Class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
-        super().__init__(size, size, x, y , id)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
         """__str__ method so that it returns
         [Square] (<id>) <x>/<y> - <size>
         """
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
-                                                    self.y,
-                                                    self.size))        
+        return ("[Square] ({}) {}/{} - {}".
+                format(self.id, self.x, self.y, self.size))
