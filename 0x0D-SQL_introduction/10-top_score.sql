105ec4d7694dd8fddbbc831ca1a4b0e906f0f570
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-07-12 19:27:55 -0300

    10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -4,5 +4,5 @@
 -- Records should be ordered by score (top first)
 -- The database name will be passed as an argument of the mysql command
 
-SELECT score, name FROM second_table;
+SELECT score, name FROM second_table
 ORDER BY score DESC; 
