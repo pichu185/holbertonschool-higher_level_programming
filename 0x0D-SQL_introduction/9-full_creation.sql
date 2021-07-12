3696ca2d007f10779a26ebf1941f710204ead0ae
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-07-12 17:01:20 -0300

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -21,6 +21,9 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 INSERT INTO second_table(id, name, score)
 VALUES (1, "John", 10);
+INSERT INTO second_table
 VALUES (2, "Alex", 3);
+INSERT INTO second_table
 VALUES (3, "Bob", 14);
+INSERT INTO second_table
 VALUES (4, "George", 8);
