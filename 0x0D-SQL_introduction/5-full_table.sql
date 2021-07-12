704f212e1c7118b47b13976ab53d3ceb9311fac8
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-07-12 16:38:00 -0300

    5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
 
-SHOW CREATE TABLE first_table FROM hbtn_0c_0;
+SHOW CREATE TABLE first_table;
