9923d00f7125f46e409484a1974993cca3ae0a8a
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-07-13 17:37:49 -0300

    11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -7,3 +7,4 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
