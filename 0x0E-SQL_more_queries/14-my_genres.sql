46eeaea1eb855faddd45a701623f28b66273887a
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-07-13 18:17:22 -0300

    14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,3 +7,4 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
+SELECT tv_genres.name AS name FROM tv_show_genres INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id AND tv_shows.title = 'Dexter' GROUP BY tv_genres.name ORDER BY name;
