fd33d72b73e772fad1e6709257a146afabc2349c
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-02 19:04:48 -0300

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin / node
-require('process');
-const len = process.argv.length;
-if (len === 2) {
-        console.log('No argument');
-} else if (len === 3) {
-        console.log('Argument found');
+const length = process.argv.length;
+
+if (length === 2) {
+  console.log('No argument');
+} else if (length === 3) {
+  console.log('Argument found');
 } else {
-        console.log('Arguments found');
+  console.log('Arguments found');
 }
