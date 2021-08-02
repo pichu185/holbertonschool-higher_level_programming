77acaa76b21e91a04749e0a67da19c60b7a6a798
pichu185 <pichu_otegui@hotmail.com>
2021-08-02 23:06:10 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,6 +5,6 @@ if (size === undefined) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+    console.log('x'.repeat(size));
   }
 }
