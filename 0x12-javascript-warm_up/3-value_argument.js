0385162b55db54fb7fa78b85c68fd4fa2a7fdad7
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-02 19:14:38 -0300

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const arguments = process.argv[2];
-if (!arguments) {
+const argv = process.argv[2];
+if (!argv) {
 	console.log('No argument');
 } else {
-  console.log(argv);
+	console.log(argv);
 }
