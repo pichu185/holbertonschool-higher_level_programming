b8f6f861ba455ac8e42d310b7ada766524ea7fae
pichu185 <pichu_otegui@hotmail.com>
2021-08-02 23:05:05 +0000

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+if (size === undefined) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
