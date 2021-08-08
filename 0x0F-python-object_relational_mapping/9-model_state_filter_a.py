4b95ba8aa8d9ed48b132a54f72b9a700999a581f
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-08 17:20:42 -0300

    9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,3 +17,14 @@ at port 3306
 import sqlalchemy
 from sys import argv
 from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+        )
+    Base.metadata.create_all(engine)
+    session = sqlalchemy.orm.Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+    session.close()
