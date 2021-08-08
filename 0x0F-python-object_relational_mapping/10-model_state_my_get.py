22757741fdcac71539682cf6eb887529f2befb15
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-08 17:21:17 -0300

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,3 +18,15 @@ at port 3306
 import sqlalchemy
 from sys import argv
 from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+        )
+    Base.metadata.create_all(engine)
+    session = sqlalchemy.orm.Session(engine)
+    try:
+        print(session.query(State).filter_by(name=argv[4]).all()[0].id)
+    except:
+        print('Not found')
+    session.close()
