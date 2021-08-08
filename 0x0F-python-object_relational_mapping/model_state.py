2a1fc978fa9448bf76cec50869fd8f4333274fd0
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-08 12:21:14 -0300

    6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,5 +15,11 @@ WARNING: all classes who inherit from Base must be imported before
 calling Base.metadata.create_all(engine)
 """
 
-import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
+import sys
+from model_state import Base
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
