371dd3d1a8323f5b9cba87add0e6b7dea3f589a8
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-08 12:17:45 -0300

    5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,3 +15,20 @@ database name and state name (SQL injection free!)
 
 from sys import argv
 import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                              passwd=argv[2], db=argv[3], port=3306,
+                              charset="utf8")
+	cursor = db.cursor()
+	cursor.execute("SELECT cities.name FROM cities INNER JOIN states\
+        ON cities.state_id = states.id WHERE states.name = %s", (argv[4],))
+	querry = cursor.fetchall()
+	if len(querry) == 0:
+		print('')
+	for i in range(len(querry)):
+		if i != (len(querry) - 1):
+			print("{}, ".format(querry[i][0]), end="")
+		else:
+			print(querry[i][0])
+	db.close()
