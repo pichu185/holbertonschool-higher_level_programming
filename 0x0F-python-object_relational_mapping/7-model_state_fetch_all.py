92c2cbb9722a4b5c4d1d682904ef42b006e18912
pichu185 <pichu_otegui@hotmail.com>
2021-08-09 00:43:45 +0000

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ and database name
 - You must use the module SQLAlchemy
 - You must import State and Base from model_state - from model_state
  import Base, State
-- Your script should connect to a MySQL server running on 
+- Your script should connect to a MySQL server running on
 localhost at port 3306
 - Results must be sorted in ascending order by states.id
 - The results must be displayed as they are in the example below
