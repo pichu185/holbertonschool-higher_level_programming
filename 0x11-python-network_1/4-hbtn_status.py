1bc99d31f4e9d1850d1b15b7c62cd22c95c75387
Pichu185 <pichu_otegui@hotmail.com>
2021-08-20 21:53:21 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''Write a Python script that fetches https://intranet.hbtn.io/status
 '''
-
+Write a Python script that fetches https://intranet.hbtn.io/status
+'''
 import requests
 
 if __name__ == "__main__":
