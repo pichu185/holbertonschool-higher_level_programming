d7f1b147bfd1075eef0b54a71974fa59400c7994
Pichu185 <pichu_otegui@hotmail.com>
2021-08-20 22:05:29 +0000

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     req = requests.get(argv[1])
-        if req.status_code < 400:
-                print(req.text)
-        else:
-                print("Error code: {}".format(req.status_code))
+    if req.status_code < 400:
+        print(req.text)
+    else:
+        print("Error code: {}".format(req.status_code))
