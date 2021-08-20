b45a6f0a4e35b331a3420c498569a4909e1883f5
Pichu185 <pichu_otegui@hotmail.com>
2021-08-20 19:42:46 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-	with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
-		html = resp.read()
-		print("Body response:")
-		print("\t- type:", type(html))
-		print("\t- content:", html)
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
+        html = resp.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
         print("\t- utf8 content:", html.decode('utf-8'))
