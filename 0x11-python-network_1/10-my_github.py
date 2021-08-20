9c9c66bea7dd2bd0f9230934eeb5813c24a1fdf7
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-20 19:36:17 -0300

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-'''
-Write a Python script that takes in a URL, sends a request to the URL and 
-displays the value of the X-Request-Id variable found in the header of 
-the response.
-'''
+"""
+Write a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id"""
 
-import urllib.request
+import requests
+from requests.auth import HTTPBasicAuth
 from sys import argv
 
-if __name__ == "__main__":
-	req = urllib.request.Request(argv[1])
-	with urllib.request.urlopen(req) as resp:
-		info = response.info()
-	print(info.get('X-Request-Id'))
+if __name__ == '__main__':
+    res = requests.get('https://api.github.com/user',
+                        auth=HTTPBasicAuth(argv[1], argv[2]))
+    if res.status_code == 200:
+        dictionary = res.json()
+        print(dictionary['id'])
+    else:
+        print(None)
