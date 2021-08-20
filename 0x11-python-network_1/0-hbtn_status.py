0673d971b5acc1c7b490c6c95d6e26958423e338
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-20 16:38:57 -0300

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''Write a Python script that fetches https://intranet.hbtn.io/status'''
+
+import urllib.request
+
+if __name__ == "__main__":
+	with urllib.request.urlopen("https://intranet.hbtn.io/status") as resp:
+		html = resp.read()
+		print("Body response:")
+		print("\t- type:", type(html))
+		print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
