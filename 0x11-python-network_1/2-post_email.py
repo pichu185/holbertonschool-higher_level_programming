5ce0ba685a8de3db4f692670fb008dd8feb6cf67
Pichu185 <pichu_otegui@hotmail.com>
2021-08-20 20:16:20 +0000

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Write a Python script that takes in a URL and an email, sends a POST 
+'''Write a Python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a parameter, and displays the
 body of the response (decoded in utf-8)
 '''
@@ -9,9 +9,9 @@ import urllib.parse
 from sys import argv
 
 if __name__ == "__main__":
-	if sys.argv[1] and sys.argv[2]:
-		data = urllib.parse.urlencode({'email': sys.argv[2]})
-		data = data.encode('ascii')
-		req = urllib.request.Request(sys.argv[1])
-		with urllib.request.urlopen(req, data) as res:
-			print(res.read().decode('utf-8'))
+    if sys.argv[1] and sys.argv[2]:
+        data = urllib.parse.urlencode({'email': sys.argv[2]})
+        data = data.encode('ascii')
+        req = urllib.request.Request(sys.argv[1])
+        with urllib.request.urlopen(req, data) as res:
+            print(res.read().decode('utf-8'))
