d8bc39b07a26a6a81b56c5fc216d6629f2e62ae2
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-20 18:59:07 -0300

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+'''
+Write a Python script that fetches https://intranet.hbtn.io/status
+'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    mail = {'email': argv[2]}
+    req = requests.post(argv[1], data=mail)
+    print(req.text)
