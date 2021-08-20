1a16e10fcd29e75978b825d71cbf1d731d9c9659
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-08-20 19:03:50 -0300

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''
+Write a Python script that fetches https://intranet.hbtn.io/status
+'''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+        if req.status_code < 400:
+                print(req.text)
+        else:
+                print("Error code: {}".format(req.status_code))
