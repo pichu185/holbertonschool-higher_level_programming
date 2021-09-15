fc85078865b97815befd680341f397a9e63d71f1
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-14 21:43:00 -0300

    2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 
 const request = require('request');
 request(process.argv[2], function (err, response) {
-        if (err) console.log(err);
-        else console.log('code: ' + response.statusCode);
+  if (err) console.log(err);
+  else console.log('code: ' + response.statusCode);
 });
