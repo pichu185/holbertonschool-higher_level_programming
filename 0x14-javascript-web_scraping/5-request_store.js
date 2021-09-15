63812d3db40e1342a1edbf0b9fe406775bea3054
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-14 22:04:18 -0300

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const path = process.argv[2];
+request(path, function (err, res, body) {
+  if (err) console.log(err);
+  else {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (err) {
+      if (err) console.log(err);
+    });
+  }
+});
