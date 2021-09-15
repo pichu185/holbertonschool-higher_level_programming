0c94158d528b30ffcc17d566d559b81756060ea4
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-14 21:57:16 -0300

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, res, body) {
+  if (err) console.log(err);
+  else if (res.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let pichu = 0;
+    for (const film in films) {
+      const chts = films[film].chts;
+      for (const char in chts) {
+        if (chts[char].includes('18')) {
+          pichu++;
+        }
+      }
+    }
+    console.log(pichu);
+  }
+});
