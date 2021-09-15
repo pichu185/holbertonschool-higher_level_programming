0d07e9d091db543da97eb6f9bb847ac08f78551e
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-14 21:47:13 -0300

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const request = require('request');
+request(url, function (err, response, body) {
+  if (err) console.log(err);
+  else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  }
+});
